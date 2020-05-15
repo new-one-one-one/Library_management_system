45459559499d9e6717749158b0c573eac0addcf4
new-one-one-one <58629311+new-one-one-one@users.noreply.github.com>
2020-05-15 14:09:54 +0530

    Addition of books and their details

--- dashboard.php
+++ dashboard.php
@@ -2,7 +2,7 @@
 session_start();
 error_reporting(0);
 include('includes/config.php');
-if(strlen($_SESSION['login'])==0)
+if(strlen($_SESSION['alogin'])==0)
   { 
 header('location:index.php');
 }
@@ -14,7 +14,10 @@ else{?>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Online Library Management System | User Dash Board</title>
+    <!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <![endif]-->
+    <title>Online Library Management System | Admin Dash Board</title>
     <!-- BOOTSTRAP CORE STYLE  -->
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
     <!-- FONT AWESOME STYLE  -->
@@ -41,24 +44,37 @@ else{?>
              
              <div class="row">
 
+ <div class="col-md-3 col-sm-3 col-xs-6">
+                      <div class="alert alert-success back-widget-set text-center">
+                            <i class="fa fa-book fa-5x"></i>
+<?php 
+$sql ="SELECT id from tblbooks ";
+$query = $dbh -> prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$listdbooks=$query->rowCount();
+?>
 
+
+                            <h3><?php echo htmlentities($listdbooks);?></h3>
+                      Books Listed
+                        </div>
+                    </div>
 
             
                  <div class="col-md-3 col-sm-3 col-xs-6">
                       <div class="alert alert-info back-widget-set text-center">
                             <i class="fa fa-bars fa-5x"></i>
 <?php 
-$sid=$_SESSION['stdid'];
-$sql1 ="SELECT id from tblissuedbookdetails where StudentID=:sid";
+$sql1 ="SELECT id from tblissuedbookdetails ";
 $query1 = $dbh -> prepare($sql1);
-$query1->bindParam(':sid',$sid,PDO::PARAM_STR);
 $query1->execute();
 $results1=$query1->fetchAll(PDO::FETCH_OBJ);
 $issuedbooks=$query1->rowCount();
 ?>
 
                             <h3><?php echo htmlentities($issuedbooks);?> </h3>
-                            Book Issued
+                           Times Book Issued
                         </div>
                     </div>
              
@@ -66,23 +82,138 @@ $issuedbooks=$query1->rowCount();
                       <div class="alert alert-warning back-widget-set text-center">
                             <i class="fa fa-recycle fa-5x"></i>
 <?php 
-$rsts=0;
-$sql2 ="SELECT id from tblissuedbookdetails where StudentID=:sid and RetrunStatus=:rsts";
+$status=1;
+$sql2 ="SELECT id from tblissuedbookdetails where RetrunStatus=:status";
 $query2 = $dbh -> prepare($sql2);
-$query2->bindParam(':sid',$sid,PDO::PARAM_STR);
-$query2->bindParam(':rsts',$rsts,PDO::PARAM_STR);
+$query2->bindParam(':status',$status,PDO::PARAM_STR);
 $query2->execute();
 $results2=$query2->fetchAll(PDO::FETCH_OBJ);
 $returnedbooks=$query2->rowCount();
 ?>
 
                             <h3><?php echo htmlentities($returnedbooks);?></h3>
-                          Books Not Returned Yet
+                          Times  Books Returned
                         </div>
                     </div>
+               <div class="col-md-3 col-sm-3 col-xs-6">
+                      <div class="alert alert-danger back-widget-set text-center">
+                            <i class="fa fa-users fa-5x"></i>
+                            <?php 
+$sql3 ="SELECT id from tblstudents ";
+$query3 = $dbh -> prepare($sql1);
+$query3->execute();
+$results3=$query3->fetchAll(PDO::FETCH_OBJ);
+$regstds=$query3->rowCount();
+?>
+                            <h3><?php echo htmlentities($regstds);?></h3>
+                           Registered Users
+                        </div>
+                    </div>
+
         </div>
 
 
+
+ <div class="row">
+
+ <div class="col-md-3 col-sm-3 col-xs-6">
+                      <div class="alert alert-success back-widget-set text-center">
+                            <i class="fa fa-user fa-5x"></i>
+<?php 
+$sq4 ="SELECT id from tblauthors ";
+$query4 = $dbh -> prepare($sql);
+$query4->execute();
+$results4=$query4->fetchAll(PDO::FETCH_OBJ);
+$listdathrs=$query4->rowCount();
+?>
+
+
+                            <h3><?php echo htmlentities($listdathrs);?></h3>
+                      Authors Listed
+                        </div>
+                    </div>
+
+            
+                 <div class="col-md-3 col-sm-3 rscol-xs-6">
+                      <div class="alert alert-info back-widget-set text-center">
+                            <i class="fa fa-file-archive-o fa-5x"></i>
+<?php 
+$sql5 ="SELECT id from tblcategory ";
+$query5 = $dbh -> prepare($sql1);
+$query5->execute();
+$results5=$query5->fetchAll(PDO::FETCH_OBJ);
+$listdcats=$query5->rowCount();
+?>
+
+                            <h3><?php echo htmlentities($listdcats);?> </h3>
+                           Listed Categories
+                        </div>
+                    </div>
+             
+
+        </div>             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+             <div class="row">
+
+              <div class="col-md-10 col-sm-8 col-xs-12 col-md-offset-1">
+                    <div id="carousel-example" class="carousel slide slide-bdr" data-ride="carousel" >
+                   
+                    <div class="carousel-inner">
+                        <div class="item active">
+
+                            <img src="assets/img/1.jpg" alt="" />
+                           
+                        </div>
+                        <div class="item">
+                            <img src="assets/img/2.jpg" alt="" />
+                          
+                        </div>
+                        <div class="item">
+                            <img src="assets/img/3.jpg" alt="" />
+                           
+                        </div>
+                    </div>
+                    <!--INDICATORS-->
+                     <ol class="carousel-indicators">
+                        <li data-target="#carousel-example" data-slide-to="0" class="active"></li>
+                        <li data-target="#carousel-example" data-slide-to="1"></li>
+                        <li data-target="#carousel-example" data-slide-to="2"></li>
+                    </ol>
+                    <!--PREVIUS-NEXT BUTTONS-->
+                     <a class="left carousel-control" href="#carousel-example" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </a>
+                </div>
+              </div>
+                 
+               
+             
+               
+            
+             </div>
             
     </div>
     </div>
